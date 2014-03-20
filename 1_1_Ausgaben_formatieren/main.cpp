5820b89f535c27fe124aaf645a78037fd2b6cd37
Marvin Wiegand <mflipw.github@gmx.de>
2014-03-20 23:49:23 +0100

    initializing

---
+++ 1_1_Ausgaben_formatieren/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  1_1_Ausgaben_formatieren
+//
+//  Created by Marvin Wiegand on 20.03.14.
+//  Copyright (c) 2014 Marvin Wiegand. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
